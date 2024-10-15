28278caecc7bd91b3284fec106e2309bb8e551b4
gabs <gabrieldasilvadev@gmail.com>
2024-10-15 03:37:33 -0300

    create relation on entities

--- dataaccess/src/main/java/br/com/liftrecord/dataaccess/tables/AddressTable.java
+++ dataaccess/src/main/java/br/com/liftrecord/dataaccess/tables/AddressTable.java
@@ -1,9 +1,14 @@
 package br.com.liftrecord.dataaccess.tables;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "address")
 public class AddressTable {
+  @Id
   private String id;
   private String state;
   private String city;
@@ -12,4 +17,73 @@ public class AddressTable {
   private String number;
   private String complement;
   private String zipCode;
+  @OneToOne(mappedBy = "address")
+  private StudentTable student;
+
+  public AddressTable() {
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public String getNeighborhood() {
+    return neighborhood;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public String getNumber() {
+    return number;
+  }
+
+  public String getComplement() {
+    return complement;
+  }
+
+  public String getZipCode() {
+    return zipCode;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public void setNeighborhood(String neighborhood) {
+    this.neighborhood = neighborhood;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  public void setNumber(String number) {
+    this.number = number;
+  }
+
+  public void setComplement(String complement) {
+    this.complement = complement;
+  }
+
+  public void setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+  }
 }
--- dataaccess/src/main/java/br/com/liftrecord/dataaccess/tables/StudentTable.java
+++ dataaccess/src/main/java/br/com/liftrecord/dataaccess/tables/StudentTable.java
@@ -1,10 +1,104 @@
 package br.com.liftrecord.dataaccess.tables;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "student")
 public class StudentTable {
+
+  @Id
   private String id;
   private String name;
   private String email;
   private String cellphone;
-//  private AddressTable addressTable;
-//  private BodyMetricsTable bodyMetricsTable;
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "address_id", referencedColumnName = "id")
+  private AddressTable address;
+  @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<BodyMetricsTable> bodyMetrics;
+
+  private StudentTable(Builder builder) {
+    id = builder.id;
+    name = builder.name;
+    email = builder.email;
+    cellphone = builder.cellphone;
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+
+  public static final class Builder {
+    private String id;
+    private String name;
+    private String email;
+    private String cellphone;
+
+    private Builder() {
+    }
+
+    public Builder id(String val) {
+      id = val;
+      return this;
+    }
+
+    public Builder name(String val) {
+      name = val;
+      return this;
+    }
+
+    public Builder email(String val) {
+      email = val;
+      return this;
+    }
+
+    public Builder cellphone(String val) {
+      cellphone = val;
+      return this;
+    }
+
+    public StudentTable build() {
+      return new StudentTable(this);
+    }
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getCellphone() {
+    return cellphone;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setCellphone(String cellphone) {
+    this.cellphone = cellphone;
+  }
 }
