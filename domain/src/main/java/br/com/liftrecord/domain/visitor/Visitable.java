78fbbdad8344949f184bc14bb1a583af0f48d1e9
gabs <gabrieldasilvadev@gmail.com>
2024-10-16 03:44:23 -0300

    refactor visitor

--- domain/src/main/java/br/com/liftrecord/domain/visitor/Visitable.java
+++ domain/src/main/java/br/com/liftrecord/domain/visitor/Visitable.java
@@ -1,5 +1,5 @@
 package br.com.liftrecord.domain.visitor;
 
-public abstract class Visitable {
-  public abstract void accept(Visitor visitor);
+public interface Visitable {
+  void accept(Visitor visitor);
 }
