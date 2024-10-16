2241086db179109e6ac8d3aa37d0c67ade369250
gabs <gabrieldasilvadev@gmail.com>
2024-10-16 03:35:46 -0300

    move dataaccess module for infrastructure

--- infrastructure/dataaccess/src/main/java/br/com/liftrecord/dataaccess/repositories/StudentRepository.java
+++ infrastructure/dataaccess/src/main/java/br/com/liftrecord/dataaccess/repositories/StudentRepository.java
@@ -4,6 +4,6 @@ import br.com.liftrecord.dataaccess.tables.StudentTable;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jpa.repository.JpaRepository;
 
-@Repository
+@Repository()
 public interface StudentRepository extends JpaRepository<StudentTable, String> {
 }
