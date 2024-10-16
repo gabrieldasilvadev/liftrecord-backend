138b47629fa10ebba639a1be1609b34d5637fab1
gabs <gabrieldasilvadev@gmail.com>
2024-10-15 22:54:34 -0300

    create module for component tests

---
+++ component-test/src/test/java/br/com/liftrecord/utils/JsonReader.java
@@ -0,0 +1,25 @@
+package br.com.liftrecord.utils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JsonReader {
+  public static final String SRC_TEST_RESOURCES_STUBS = "src/test/resources/stubs/";
+
+  public static String readJsonRequest(String path) {
+    try {
+      return new String(Files.readAllBytes(Paths.get(String.format(SRC_TEST_RESOURCES_STUBS + "requests/%s", path))));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static String readJsonResponse(String path) {
+    try {
+      return new String(Files.readAllBytes(Paths.get(String.format(SRC_TEST_RESOURCES_STUBS + "responses/%s", path))));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
