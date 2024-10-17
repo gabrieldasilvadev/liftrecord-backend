140863c134accd241c03d840cde30b0a1b0ab238
gabs <gabrieldasilvadev@gmail.com>
2024-10-17 03:26:44 -0300

    update post student method

--- api/src/main/java/br/com/liftrecord/api/controllers/student/StudentController.java
+++ api/src/main/java/br/com/liftrecord/api/controllers/student/StudentController.java
@@ -4,7 +4,6 @@ import br.com.liftrecord.api.controllers.student.mappers.RegisterStudentControll
 import br.com.liftrecord.contract.api.StudentApi;
 import br.com.liftrecord.contract.model.RegisterStudentRequest;
 import br.com.liftrecord.contract.model.RegisterStudentResponse;
-import br.com.liftrecord.domain.student.Student;
 import br.com.liftrecord.shared.mediator.Mediator;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
@@ -22,7 +21,6 @@ public class StudentController implements StudentApi {
   @Override
   public HttpResponse<@Valid RegisterStudentResponse> postStudents(RegisterStudentRequest registerStudentRequest) {
     RegisterStudentControllerMapper mapper = RegisterStudentControllerMapper.INSTANCE;
-    final Student student = mediator.execute(mapper.toCommand(registerStudentRequest));
-    return HttpResponse.created(mapper.toResponse(student));
+    return HttpResponse.created(mapper.toResponse(mediator.execute(mapper.toCommand(registerStudentRequest))));
   }
 }
