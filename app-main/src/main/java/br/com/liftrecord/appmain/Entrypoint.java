f9da06f8f62d417bec20e3c1cb87de67c7155326
gabs <gabrieldasilvadev@gmail.com>
2024-10-17 03:01:20 -0300

    update entrypoint

--- app-main/src/main/java/br/com/liftrecord/appmain/Entrypoint.java
+++ app-main/src/main/java/br/com/liftrecord/appmain/Entrypoint.java
@@ -4,7 +4,7 @@ import io.micronaut.core.annotation.Introspected;
 import io.micronaut.runtime.Micronaut;
 import jakarta.persistence.Entity;
 
-@Introspected(packages = "br.com.liftrecord.dataaccess", includedAnnotations = Entity.class)
+@Introspected(packages = "br.com.liftrecord.dataaccess.tables", includedAnnotations = Entity.class)
 public class Entrypoint {
   public static void main(String[] args) {
     Micronaut.run(Entrypoint.class, args);
