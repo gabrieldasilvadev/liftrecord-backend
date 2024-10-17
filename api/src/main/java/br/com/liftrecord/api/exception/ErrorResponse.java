46d5db3b14eb4a043977eb31b73a7adb46a6a462
gabs <gabrieldasilvadev@gmail.com>
2024-10-17 03:00:45 -0300

    update ErrorResponse

--- api/src/main/java/br/com/liftrecord/api/exception/ErrorResponse.java
+++ api/src/main/java/br/com/liftrecord/api/exception/ErrorResponse.java
@@ -1,5 +1,8 @@
 package br.com.liftrecord.api.exception;
 
+import io.micronaut.serde.annotation.Serdeable;
+
+@Serdeable
 public record ErrorResponse(int status, String message) {
     public static ErrorResponse of(int status, String message) {
         return new ErrorResponse(status, message);
