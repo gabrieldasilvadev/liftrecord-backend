9015085746557172c9b135ac3e926cc02ed1d51e
gabs <gabrieldasilvadev@gmail.com>
2024-10-17 03:01:12 -0300

    created AddressTableMapper

---
+++ infrastructure/dataaccess/src/main/java/br/com/liftrecord/dataaccess/mappers/AddressTableMapper.java
@@ -0,0 +1,23 @@
+package br.com.liftrecord.dataaccess.mappers;
+
+import br.com.liftrecord.dataaccess.tables.AddressTable;
+import br.com.liftrecord.domain.student.valueobjects.Address;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AddressTableMapper {
+  AddressTableMapper INSTANCE = Mappers.getMapper(AddressTableMapper.class);
+
+  default AddressTable toTable(Address address) {
+    return AddressTable.builder()
+        .street(address.getStreet())
+        .number(address.getNumber())
+        .neighborhood(address.getNeighborhood())
+        .complement(address.getComplement())
+        .city(address.getCity())
+        .state(address.getState())
+        .zipCode(address.getZipCode())
+        .build();
+  }
+}
