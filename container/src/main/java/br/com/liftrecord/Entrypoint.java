45d9614f28d350def78f0611fa7e0ea0774ed146
gabs <gabrieldasilvadev@gmail.com>
2024-10-22 02:32:23 -0300

    add annotations for scan packages in entrypoint

--- container/src/main/java/br/com/liftrecord/Entrypoint.java
+++ container/src/main/java/br/com/liftrecord/Entrypoint.java
@@ -3,8 +3,9 @@ package br.com.liftrecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EnableJpaRepositories(basePackages = "br.com.liftrecord.repositories")
+@EnableJpaRepositories(basePackages = "br.com.liftrecord.repositories")
 @EntityScan(basePackages = "br.com.liftrecord.tables")
 @SpringBootApplication(scanBasePackages = "br.com.liftrecord")
 public class Entrypoint {
