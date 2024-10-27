8121c8b37d24f32a09ffa45c25ebd817816fb775
gabs <gabrieldasilvadev@gmail.com>
2024-10-27 03:31:41 -0300

    created component test status code 400 invalid email pattern

--- component-test/src/test/java/br/com/liftrecord/RegisterStudentComponentTest.java
+++ component-test/src/test/java/br/com/liftrecord/RegisterStudentComponentTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -29,5 +30,21 @@ public class RegisterStudentComponentTest extends BaseComponentTest {
         .body("name", equalTo("name_bbaa851b2153"))
         .body("email", equalTo("emailexample@gmail.com"))
         .body("status", equalTo("pending_activation"));
+  }
+
+  @Test
+  @DisplayName("Given a valid request, When POST /students, Then return status 400 (Bad Request) invalid Email pattern")
+  public void testRegisterInvalidEmailPattern() {
+
+    String jsonRequest = JsonReaderUtil.readJsonRequest("post-register-student-invalid-email-pattern-status-400.json");
+
+    given()
+        .body(jsonRequest)
+        .contentType(ContentType.JSON)
+        .when()
+        .post("/students/")
+        .then()
+        .statusCode(HttpStatus.SC_BAD_REQUEST)
+        .body("message", containsStringIgnoringCase("Email pattern is invalid"));
   }
 }
