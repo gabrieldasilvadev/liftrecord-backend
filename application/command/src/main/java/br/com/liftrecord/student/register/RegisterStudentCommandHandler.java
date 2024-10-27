f5326c1e11745433318ecc3535793c3f5dc5cda4
gabs <gabrieldasilvadev@gmail.com>
2024-10-27 03:30:34 -0300

    created validator for register student

--- application/command/src/main/java/br/com/liftrecord/student/register/RegisterStudentCommandHandler.java
+++ application/command/src/main/java/br/com/liftrecord/student/register/RegisterStudentCommandHandler.java
@@ -1,7 +1,11 @@
-package br.com.liftrecord.student;
+package br.com.liftrecord.student.register;
 
+import br.com.liftrecord.Log5WBuilder;
+import br.com.liftrecord.student.register.validator.RegisterStudentValidationException;
 import br.com.liftrecord.SaveStudentUseCase;
 import br.com.liftrecord.command.CommandHandler;
+import br.com.liftrecord.student.Student;
+import br.com.liftrecord.student.register.validator.RegisterStudentValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -11,6 +15,7 @@ public class RegisterStudentCommandHandler implements CommandHandler<RegisterStu
 
   private static final Logger logger = LoggerFactory.getLogger(RegisterStudentCommandHandler.class);
   private final SaveStudentUseCase saveStudentUseCase;
+  private final RegisterStudentValidator registerStudentValidator = new RegisterStudentValidator();
 
   public RegisterStudentCommandHandler(SaveStudentUseCase saveStudentUseCase) {
     this.saveStudentUseCase = saveStudentUseCase;
@@ -18,7 +23,15 @@ public class RegisterStudentCommandHandler implements CommandHandler<RegisterStu
 
   @Override
   public Student handle(final RegisterStudentCommand command) {
-    logger.info("RegisterStudentCommandHandler.handle: {}", command);
+    Log5WBuilder
+        .method()
+        .logCode("RSCH-HDL-001")
+        .whatHappen("Register student")
+        .addInfo("email", command.getEmail())
+        .info(logger);
+
+    registerStudentValidator.validate(command).isInvalidThrow(RegisterStudentValidationException.class);
+
     final Student student = command.toStudent();
     return saveStudentUseCase.execute(student);
   }
---
+++ application/command/src/main/java/br/com/liftrecord/student/register/validator/RegisterStudentValidator.java
@@ -0,0 +1,32 @@
+package br.com.liftrecord.student.register.validator;
+
+import br.com.fluentvalidator.AbstractValidator;
+import br.com.fluentvalidator.predicate.PredicateBuilder;
+import br.com.liftrecord.student.register.RegisterStudentCommand;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public class RegisterStudentValidator extends AbstractValidator<RegisterStudentCommand> {
+
+  private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+  Pattern pattern = Pattern.compile(EMAIL_REGEX);
+
+  @Override
+  public void rules() {
+
+    ruleFor(RegisterStudentCommand::getName)
+        .must(name -> !name.isEmpty())
+        .when(String::isBlank)
+        .withCode("NAME_IS_BLANK")
+        .withMessage("Name cannot be blank")
+        .withFieldName("name")
+        .critical();
+
+    ruleFor(RegisterStudentCommand::getEmail)
+        .must(PredicateBuilder.<String>from(email -> email != null && pattern.matcher(email).matches()))  // Valida com regex
+        .withCode("EMAIL_PATTERN_INVALID")
+        .withMessage("Email pattern is invalid")
+        .withFieldName("email")
+        .critical();
+  }
+}
