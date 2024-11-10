993645e3a01d9c6e085357da4973a8ee78b2d617
gabs <gabrieldasilvadev@gmail.com>
2024-11-10 15:27:25 -0300

    update entrypoint

--- container/src/main/java/br/com/liftrecord/Entrypoint.java
+++ container/src/main/java/br/com/liftrecord/Entrypoint.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "br.com.liftrecord.repositories")
 @EntityScan(basePackages = "br.com.liftrecord.tables")
-@SpringBootApplication(scanBasePackages = "br.com.liftrecord")
+@SpringBootApplication
 public class Entrypoint {
   public static void main(String[] args) {
     SpringApplication.run(Entrypoint.class, args);
