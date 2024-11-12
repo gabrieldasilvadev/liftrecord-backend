3c64ffae828ee1799cf2389103467c268da20799
gabs <gabrieldasilvadev@gmail.com>
2024-11-11 21:18:55 -0300

    refactor http exception handler

--- common/common-http-exception/src/main/java/br/com/liftrecord/HttpExceptionHandler.java
+++ common/common-http-exception/src/main/java/br/com/liftrecord/HttpExceptionHandler.java
@@ -1,8 +1,11 @@
 package br.com.liftrecord;
 
+import br.com.liftrecord.exception.AccountAlreadyExistsException;
 import br.com.liftrecord.exception.ObjectStateException;
 import br.com.liftrecord.openapi.model.ErrorResponseDto;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Objects;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,24 +17,32 @@ public class HttpExceptionHandler {
 
   @ExceptionHandler(RuntimeException.class)
   public ResponseEntity<ErrorResponseDto> handleException(Exception ex, HttpServletRequest request) {
-    ErrorResponseDto errorResponse = ErrorResponseDto.builder()
-        .message(ex.getMessage())
-        .status(HttpStatus.INTERNAL_SERVER_ERROR.name())
-        .uri(request.getRequestURI())
-        .build();
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    return buildErrorResponse(ex, request, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
   @ExceptionHandler(value = {
       IllegalArgumentException.class,
       MethodArgumentNotValidException.class,
-      ObjectStateException.class})
+      ObjectStateException.class
+  })
   public ResponseEntity<ErrorResponseDto> handleBadRequestException(Exception ex, HttpServletRequest request) {
+    return buildErrorResponse(ex, request, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(value = {
+      AccountAlreadyExistsException.class
+  })
+  public ResponseEntity<ErrorResponseDto> handleAccountAlreadyExistsException(Exception ex, HttpServletRequest request) {
+    return buildErrorResponse(ex, request, HttpStatus.CONFLICT);
+  }
+
+  private ResponseEntity<ErrorResponseDto> buildErrorResponse(Exception ex, HttpServletRequest request, HttpStatus status) {
+    String message = ObjectUtils.firstNonNull(ex.getMessage(), "Unknown error: " + ex.getClass().getName());
     ErrorResponseDto errorResponse = ErrorResponseDto.builder()
-        .message(ex.getMessage())
-        .status(HttpStatus.BAD_REQUEST.name())
+        .message(message)
+        .status(status.name())
         .uri(request.getRequestURI())
         .build();
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    return ResponseEntity.status(status).body(errorResponse);
   }
 }
