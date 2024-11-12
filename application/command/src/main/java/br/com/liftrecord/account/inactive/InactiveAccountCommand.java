77cfce7dfea6b0270eef1d01a0944bc1d10395e6
gabs <gabrieldasilvadev@gmail.com>
2024-11-12 02:15:27 -0300

    added jakarta validations

--- application/command/src/main/java/br/com/liftrecord/account/inactive/InactiveAccountCommand.java
+++ application/command/src/main/java/br/com/liftrecord/account/inactive/InactiveAccountCommand.java
@@ -1,10 +1,12 @@
 package br.com.liftrecord.account.inactive;
 
 import br.com.liftrecord.command.Command;
+import jakarta.validation.constraints.Size;
 import java.io.Serial;
 import java.io.Serializable;
 
 public record InactiveAccountCommand(
+    @Size(min = 26, max = 26)
     String accountId
 ) implements Command, Serializable {
 
--- application/command/src/main/java/br/com/liftrecord/account/inactive/InactiveAccountCommandHandler.java
+++ application/command/src/main/java/br/com/liftrecord/account/inactive/InactiveAccountCommandHandler.java
@@ -2,10 +2,11 @@ package br.com.liftrecord.account.inactive;
 
 import br.com.liftrecord.InactiveAccountUseCase;
 import br.com.liftrecord.account.Account;
-import br.com.liftrecord.account.AccountId;
+import br.com.liftrecord.account.valueobjects.AccountId;
 import br.com.liftrecord.command.CommandHandler;
 import br.com.liftrecord.exception.ObjectStateException;
 import br.com.liftrecord.account.inactive.validator.InactiveAccountValidator;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,7 @@ public class InactiveAccountCommandHandler implements CommandHandler<InactiveAcc
   private final InactiveAccountValidator inactiveAccountValidator;
 
   @Override
-  public Account handle(InactiveAccountCommand command) {
+  public Account handle(@Valid InactiveAccountCommand command) {
     inactiveAccountValidator.validate(command).isInvalidThrow(ObjectStateException.class);
     return inactiveAccountUseCase.execute(AccountId.fromString(command.accountId()));
   }
