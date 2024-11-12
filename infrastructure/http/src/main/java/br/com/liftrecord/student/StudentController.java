cb00f0a093866579d1d0e0f49a361519f54961a1
gabs <gabrieldasilvadev@gmail.com>
2024-11-12 02:16:02 -0300

    created method post assign body metrics

--- infrastructure/http/src/main/java/br/com/liftrecord/student/StudentController.java
+++ infrastructure/http/src/main/java/br/com/liftrecord/student/StudentController.java
@@ -1,12 +1,26 @@
 package br.com.liftrecord.student;
 
+import br.com.liftrecord.mediator.Mediator;
 import br.com.liftrecord.openapi.api.StudentApi;
+import br.com.liftrecord.openapi.model.AssignBodyMetricsRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
 public class StudentController implements StudentApi {
+
+  private final Mediator mediator;
 
   @Override
   public ResponseEntity<Void> postApiV1Students() {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Void> postApiV1StudentsStudentIdBodyMetrics(String studentId, AssignBodyMetricsRequestDto assignBodyMetricsRequestDto) {
+    mediator.execute(AssignBodyMetricsControllerMapper.toCommand(studentId, assignBodyMetricsRequestDto));
     return null;
   }
 }
