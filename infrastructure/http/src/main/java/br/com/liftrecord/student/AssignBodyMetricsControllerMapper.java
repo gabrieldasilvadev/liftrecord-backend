bb64ca7c8c9e7aa13ff7ee757e5ef653d17b879b
gabs <gabrieldasilvadev@gmail.com>
2024-11-16 12:50:56 -0300

    added response for assign body metrics route

--- infrastructure/http/src/main/java/br/com/liftrecord/student/AssignBodyMetricsControllerMapper.java
+++ infrastructure/http/src/main/java/br/com/liftrecord/student/AssignBodyMetricsControllerMapper.java
@@ -1,7 +1,12 @@
 package br.com.liftrecord.student;
 
+import br.com.liftrecord.core.DomainObject;
 import br.com.liftrecord.openapi.model.AssignBodyMetricsRequestDto;
+import br.com.liftrecord.openapi.model.AssignBodyMetricsResponseDto;
 import br.com.liftrecord.student.bodymetrics.AssignBodyMetricsCommand;
+import br.com.liftrecord.student.valueobjects.BodyMetrics;
+import java.time.LocalDate;
+import org.springframework.http.ResponseEntity;
 
 public class AssignBodyMetricsControllerMapper {
   public static AssignBodyMetricsCommand toCommand(String studentId,
@@ -18,5 +23,21 @@ public class AssignBodyMetricsControllerMapper {
         assignBodyMetricsRequestDto.getHipsCircumference(),
         assignBodyMetricsRequestDto.getBmi(),
         assignBodyMetricsRequestDto.getBasalMetabolicRate());
+  }
+
+  public static AssignBodyMetricsResponseDto toResponse(Student student) {
+    final BodyMetrics currentBodyMetrics = student.getBodyMetrics().get(LocalDate.now());
+    return AssignBodyMetricsResponseDto.builder()
+        .weight(currentBodyMetrics.getWeight())
+        .height(currentBodyMetrics.getHeight())
+        .waistCircumference(currentBodyMetrics.getWaistCircumference())
+        .abdomenCircumference(currentBodyMetrics.getAbdomenCircumference())
+        .chestCircumference(currentBodyMetrics.getChestCircumference())
+        .armCircumference(currentBodyMetrics.getArmCircumference())
+        .thighCircumference(currentBodyMetrics.getThighCircumference())
+        .hipsCircumference(currentBodyMetrics.getHipsCircumference())
+        .bmi(currentBodyMetrics.getBmi())
+        .basalMetabolicRate(currentBodyMetrics.getBasalMetabolicRate())
+        .build();
   }
 }
--- infrastructure/http/src/main/java/br/com/liftrecord/student/StudentController.java
+++ infrastructure/http/src/main/java/br/com/liftrecord/student/StudentController.java
@@ -3,7 +3,9 @@ package br.com.liftrecord.student;
 import br.com.liftrecord.mediator.Mediator;
 import br.com.liftrecord.openapi.api.StudentApi;
 import br.com.liftrecord.openapi.model.AssignBodyMetricsRequestDto;
+import br.com.liftrecord.openapi.model.AssignBodyMetricsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +21,8 @@ public class StudentController implements StudentApi {
   }
 
   @Override
-  public ResponseEntity<Void> postApiV1StudentsStudentIdBodyMetrics(String studentId, AssignBodyMetricsRequestDto assignBodyMetricsRequestDto) {
-    mediator.execute(AssignBodyMetricsControllerMapper.toCommand(studentId, assignBodyMetricsRequestDto));
-    return null;
+  public ResponseEntity<AssignBodyMetricsResponseDto> postApiV1StudentsStudentIdBodyMetrics(String studentId, AssignBodyMetricsRequestDto assignBodyMetricsRequestDto) {
+    Student student = mediator.execute(AssignBodyMetricsControllerMapper.toCommand(studentId, assignBodyMetricsRequestDto));
+    return ResponseEntity.status(HttpStatus.CREATED).body(AssignBodyMetricsControllerMapper.toResponse(student));
   }
 }
